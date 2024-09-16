391449e1d575125f60ab43ce45d0c56251a3b7a2
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2024-09-16 21:24:07 +0430

    random quote made by react

---
+++ src/QuoteFetcher.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random"
+
+function QuoteFetcher() {
+    const [ data,setData ] = useState([{ text: "All know the way; few actually walk it.", author: "Bodhidharma" }])
+    const [ loading,setLoading ] = useState(false)
+
+    async function fetchQuote(){
+        setLoading(true)
+        const response = await fetch(RANDOM_QUOTE_URL)
+        const jsonResponse = await response.json()
+        const newQuote = jsonResponse.quote
+        setData([ newQuote, ...data ])
+        setLoading(false)
+    }
+
+    function handleDelete(idx){
+        const newData = [...data]
+        newData.splice(idx,1)
+        setData(newData)
+    }
+  return (
+    <div> 
+        <h2>Quote of the Day</h2>
+        <div className='headerBtn'>
+            <button onClick={fetchQuote}>New Quote</button> <a href="https://github.com/Nawabi-Hamza">~Hamza Nawabi</a>
+        </div>
+        <hr />
+        {loading && <h4 className='Loader'>Loading...</h4> }
+        {data.map((item,idx)=>(
+            <div className='Quote' key={idx}>
+                <h3>{item.text}</h3>
+                <p>~{item.author}</p>
+                <button className='DeleteBtn' onClick={()=> handleDelete(idx)}>x</button>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default QuoteFetcher
